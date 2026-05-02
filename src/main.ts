65d048b19b2ce43ef5a88ec265a6c0c73b5be8be
Gael Mousset <gaelmousset@outlook.fr>
2026-05-02 13:33:46 +0200

    Add oversize pic

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bodyParser: false });
+
+  app.use(json({ limit: '10mb' }));
+  app.use(urlencoded({ limit: '10mb', extended: true }));
 
   app.enableCors({ origin: ['http://localhost:5173', 'http://localhost:5174'] });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
