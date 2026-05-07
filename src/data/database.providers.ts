8c8ed58a87e5ef81269edc328bf449fc6b6b3038
Gael Mousset <gaelmousset@outlook.fr>
2026-05-07 14:45:28 +0200

    env url

--- src/data/database.providers.ts
+++ src/data/database.providers.ts
@@ -3,9 +3,10 @@ import * as mongoose from 'mongoose';
 export const connectDB = [
   {
     provide: 'DATABASE_CONNECTION',
-    useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect(
-        process.env.MONGODB_URI ?? 'mongodb://localhost:27017/warrion',
-      ),
+    useFactory: (): Promise<typeof mongoose> => {
+      const uri = process.env.MONGODB_URI;
+      if (!uri) throw new Error('MONGODB_URI is not defined');
+      return mongoose.connect(uri);
+    },
   },
 ];
